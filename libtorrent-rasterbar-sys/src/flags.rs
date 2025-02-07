0f74b623af71beee2d8a72e68053a3e1f0c94f72
PeterDing <dfhayst@gmail.com>
2025-02-07 16:57:54 +0800

    use camel_case_types

--- libtorrent-rasterbar-sys/src/flags.rs
+++ libtorrent-rasterbar-sys/src/flags.rs
@@ -1,25 +1,25 @@
+#![allow(non_camel_case_types)]
+
 use bitflags::bitflags;
 
 bitflags! {
     /// libtorrent/session_handler.hpp
     /// These flags are defined in struct session_handle
     pub struct SaveStateFlags: u32 {
-        // saves settings (i.e. the settings_pack)
-        // static constexpr save_state_flags_t save_settings = 0_bit;
-        const SaveSettings = 1 << 0;
-        // saves dht state such as nodes and node-id, possibly accelerating
-        // joining the DHT if provided at next session startup.
-        // static constexpr save_state_flags_t save_dht_state = 2_bit;
-        const SaveDhtState = 1 << 2;
-        // load or save state from plugins
-        // static constexpr save_state_flags_t save_extension_state = 11_bit;
-        const SaveExtensionState = 1 << 11;
+/// saves settings (i.e. the settings_pack)
+/// static constexpr save_state_flags_t save_settings = 0_bit;
+        const save_settings = 1 << 0;
+/// saves dht state such as nodes and node-id, possibly accelerating
+/// joining the DHT if provided at next session startup.
+/// static constexpr save_state_flags_t save_dht_state = 2_bit;
+        const save_dht_state = 1 << 2;
+/// load or save state from plugins
+/// static constexpr save_state_flags_t save_extension_state = 11_bit;
+        const save_extension_state = 1 << 11;
 
-        // load or save the IP filter set on the session
-        // static constexpr save_state_flags_t save_ip_filter = 12_bit;
-        const SaveIpFilter = 1 << 12;
-
-        const All = !0;
+/// load or save the IP filter set on the session
+/// static constexpr save_state_flags_t save_ip_filter = 12_bit;
+        const save_ip_filter = 1 << 12;
     }
 }
 
@@ -43,7 +43,7 @@ bitflags! {
         /// It is not possible to *set* the ``seed_mode`` flag on a torrent after it has
         /// been added to a session. It is possible to *clear* it though.
         /// constexpr torrent_flags_t seed_mode = 0_bit;
-        const SeedMode = 0;
+        const seed_mode = 0;
 
         /// If ``upload_mode`` is set, the torrent will be initialized in
         /// upload-mode, which means it will not make any piece requests. This
@@ -60,7 +60,7 @@ bitflags! {
         /// got there. If it's important to manually control when the torrent
         /// leaves upload mode, don't make it auto managed.
         /// constexpr torrent_flags_t upload_mode = 1_bit;
-        const UploadMode = 1 << 1;
+        const upload_mode = 1 << 1;
 
         /// determines if the torrent should be added in *share mode* or not.
         /// Share mode indicates that we are not interested in downloading the
@@ -81,7 +81,7 @@ bitflags! {
         /// The share mode has one setting, the share ratio target, see
         /// ``settings_pack::share_mode_target`` for more info.
         /// constexpr torrent_flags_t share_mode = 2_bit;
-        const ShareMode = 1 << 2;
+        const share_mode = 1 << 2;
 
         /// determines if the IP filter should apply to this torrent or not. By
         /// default all torrents are subject to filtering by the IP filter
@@ -89,14 +89,14 @@ bitflags! {
         /// torrents needs to be exempt for some reason, being an auto-update
         /// torrent for instance.
         /// constexpr torrent_flags_t apply_ip_filter = 3_bit;
-        const ApplyIpFilter = 1 << 3;
+        const apply_ip_filter = 1 << 3;
 
         /// specifies whether or not the torrent is paused. i.e. it won't connect to the tracker or any of the peers
         /// until it's resumed. Note that a paused torrent that also has the
         /// auto_managed flag set can be started at any time by libtorrent's queuing
         /// logic. See queuing_.
         /// constexpr torrent_flags_t paused = 4_bit;
-        const Paused = 1 << 4;
+        const paused = 1 << 4;
 
         /// If the torrent is auto-managed (``auto_managed``), the torrent
         /// may be resumed at any point, regardless of how it paused. If it's
@@ -109,7 +109,7 @@ bitflags! {
         /// order is the order the torrents were added. They are all downloaded
         /// in that order. For more details, see queuing_.
         /// constexpr torrent_flags_t auto_managed = 5_bit;
-        const AutoManaged = 1 << 5;
+        const auto_managed = 1 << 5;
 
         /// used in add_torrent_params to indicate that it's an error to attempt
         /// to add a torrent that's already in the session. If it's not considered an
@@ -117,18 +117,18 @@ bitflags! {
         /// This flag is not saved by write_resume_data(), since it is only meant for
         /// adding torrents.
         /// constexpr torrent_flags_t duplicate_is_error = 6_bit;
-        const DuplicateIsError = 1 << 6;
+        const duplicate_is_error = 1 << 6;
 
         /// on by default and means that this torrent will be part of state
         /// updates when calling post_torrent_updates().
         /// This flag is not saved by write_resume_data().
         /// constexpr torrent_flags_t update_subscribe = 7_bit;
-        const UpdateSubscribe = 1 << 7;
+        const update_subscribe = 1 << 7;
 
         /// sets the torrent into super seeding/initial seeding mode. If the torrent
         /// is not a seed, this flag has no effect.
         /// constexpr torrent_flags_t super_seeding = 8_bit;
-        const SuperSeeding = 1 << 8;
+        const super_seeding = 1 << 8;
 
         /// sets the sequential download state for the torrent. In this mode the
         /// piece picker will pick pieces with low index numbers before pieces with
@@ -137,7 +137,7 @@ bitflags! {
         /// or "prefer whole pieces"-mode. Sequential mode is not ideal for streaming
         /// media. For that, see set_piece_deadline() instead.
         /// constexpr torrent_flags_t sequential_download = 9_bit;
-        const SequentialDownload = 1 << 9;
+        const sequential_download = 1 << 9;
 
         /// When this flag is set, the torrent will *force stop* whenever it
         /// transitions from a non-data-transferring state into a data-transferring
@@ -174,7 +174,7 @@ bitflags! {
         /// as a downloading state. In order to stop a torrent once the metadata
         /// has been downloaded, instead set all file priorities to dont_download
         /// constexpr torrent_flags_t stop_when_ready = 10_bit;
-        const StopWhenReady = 1 << 10;
+        const stop_when_ready = 1 << 10;
 
         /// when this flag is set, the tracker list in the add_torrent_params
         /// object override any trackers from the torrent file. If the flag is
@@ -187,7 +187,7 @@ bitflags! {
         /// explicitly removed in the previous session.
         /// This flag is not saved by write_resume_data()
         /// constexpr torrent_flags_t override_trackers = 11_bit;
-        const OverrideTrackers = 1 << 11;
+        const override_trackers = 1 << 11;
 
         /// If this flag is set, the web seeds from the add_torrent_params
         /// object will override any web seeds in the torrent file. If it's not
@@ -200,7 +200,7 @@ bitflags! {
         /// explicitly removed in the previous session.
         /// This flag is not saved by write_resume_data()
         /// constexpr torrent_flags_t override_web_seeds = 12_bit;
-        const OverrideWebSeeds = 1 << 12;
+        const override_web_seeds = 1 << 12;
 
         /// if this flag is set (which it is by default) the torrent will be
         /// considered needing to save its resume data immediately, in the
@@ -211,22 +211,22 @@ bitflags! {
         /// This flag is not saved by write_resume_data(), since it represents an
         /// ephemeral state of a running torrent.
         /// constexpr torrent_flags_t need_save_resume = 13_bit;
-        const NeedSaveResume = 1 << 13;
+        const need_save_resume = 1 << 13;
 
         /// set this flag to disable DHT for this torrent. This lets you have the DHT
         /// enabled for the whole client, and still have specific torrents not
         /// participating in it. i.e. not announcing to the DHT nor picking up peers
         /// from it.
         /// constexpr torrent_flags_t disable_dht = 19_bit;
-        const DisableDht = 1 << 19;
+        const disable_dht = 1 << 19;
 
         /// set this flag to disable local service discovery for this torrent.
         /// constexpr torrent_flags_t disable_lsd = 20_bit;
-        const DisableLsd = 1 << 20;
+        const disable_lsd = 1 << 20;
 
         /// set this flag to disable peer exchange for this torrent.
         /// constexpr torrent_flags_t disable_pex = 21_bit;
-        const DisablePex = 1 << 21;
+        const disable_pex = 1 << 21;
 
         /// if this flag is set, the resume data will be assumed to be correct
         /// without validating it against any files on disk. This may be used when
@@ -236,7 +236,7 @@ bitflags! {
         /// time to some save path that may already have some of the files, this flag
         /// should not be set.
         /// constexpr torrent_flags_t no_verify_files = 22_bit;
-        const NoVerifyFiles = 1 << 22;
+        const no_verify_files = 1 << 22;
 
         /// default all file priorities to dont_download. This is useful for adding
         /// magnet links where the number of files is unknown, but the
@@ -245,7 +245,7 @@ bitflags! {
         /// unless this flag is set, in which case they will be set to 0
         /// (dont_download).
         /// constexpr torrent_flags_t default_dont_download = 23_bit;
-        const DefaultDontDownload = 1 << 23;
+        const default_dont_download = 1 << 23;
 
         /// this flag makes the torrent be considered an "i2p torrent" for purposes
         /// of the allow_i2p_mixed setting. When mixing regular peers and i2p peers
@@ -258,11 +258,42 @@ bitflags! {
         /// It's also set by parse_magnet_uri() if the tracker list contains such
         /// URL.
         /// constexpr torrent_flags_t i2p_torrent = 24_bit;
-        const I2pTorrent = 1 << 24;
-
-        /// all torrent flags combined. Can conveniently be used when creating masks
-        /// for flags
-        /// constexpr torrent_flags_t all = torrent_flags_t::all();
-        const All = !0;
+        const i2p_torrent = 1 << 24;
     }
+}
+
+bitflags! {
+    /// libtorrent/torrent_flags.hpp
+    /// These flags are defined in namespace torrent_flags
+    pub struct PauseFlags: u8 {
+        /// will delay the disconnect of peers that we're still downloading
+        /// requests and will disconnect all idle peers. As soon as a peer is done
+        /// outstanding requests from. The torrent will not accept any more
+        /// transferring the blocks that were requested from it, it is
+        /// disconnected. This is a graceful shut down of the torrent in the sense
+        /// that no downloaded bytes are wasted.
+        /// static constexpr pause_flags_t graceful_pause = 0_bit;
+        const graceful_pause = 1 << 0;
+    }
+}
+
+/// libtorrent/download_priority.hpp
+#[repr(u8)]
+pub enum DownloadPriority {
+    /// Don't download the file or piece. Partial pieces may still be downloaded when
+    /// setting file priorities.
+    /// constexpr download_priority_t dont_download{0};
+    dont_download = 0,
+
+    /// The default priority for files and pieces.
+    /// constexpr download_priority_t default_priority{4};
+    default_priority = 4,
+
+    /// The lowest priority for files and pieces.
+    /// constexpr download_priority_t low_priority{1};
+    low_priority = 1,
+
+    /// The highest priority for files and pieces.
+    /// constexpr download_priority_t top_priority{7};
+    top_priority = 7,
 }
