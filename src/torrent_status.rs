52fa3c7d535662d96306a5f2220b107913554dfd
PeterDing <dfhayst@gmail.com>
2025-02-10 17:56:22 +0800

    add State enum

--- src/torrent_status.rs
+++ src/torrent_status.rs
@@ -1,5 +1,45 @@
+#![allow(non_camel_case_types)]
+
 use libtorrent_rasterbar_sys::ffi;
 use serde::Serialize;
+
+// the different overall states a torrent can be in
+#[repr(u8)]
+pub enum State {
+    // internal
+    unused_enum_for_backwards_compatibility,
+
+    // The torrent has not started its download yet, and is
+    // currently checking existing files.
+    checking_files,
+
+    // The torrent is trying to download metadata from peers.
+    // This implies the ut_metadata extension is in use.
+    downloading_metadata,
+
+    // The torrent is being downloaded. This is the state
+    // most torrents will be in most of the time. The progress
+    // meter will tell how much of the files that has been
+    // downloaded.
+    downloading,
+
+    // In this state the torrent has finished downloading but
+    // still doesn't have the entire torrent. i.e. some pieces
+    // are filtered and won't get downloaded.
+    finished,
+
+    // In this state the torrent has finished downloading and
+    // is a pure seeder.
+    seeding,
+
+    unused_enum_for_backwards_compatibility_allocating,
+
+    // The torrent is currently checking the fast resume data and
+    // comparing it to the files on disk. This is typically
+    // completed in a fraction of a second, but if you add a
+    // large number of torrents at once, they will queue up.
+    checking_resume_data,
+}
 
 /// libtorrent/torrent_status.hpp
 ///
@@ -270,6 +310,8 @@ pub struct TorrentStatus {
     pub seed_rank: i32,
 
     // the main state the torrent is in. See torrent_status::state_t.
+    //
+    // see State
     pub state: u8, // default: checking_resume_data
 
     // true if this torrent has unsaved changes
@@ -331,6 +373,8 @@ pub struct TorrentStatus {
 
     // reflects several of the torrent's flags. For more
     // information, see ``torrent_handle::flags()``.
+    //
+    // see libtorrent_rasterbar_sys::flags::TorrentFlags
     pub flags: u64,
 }
 
